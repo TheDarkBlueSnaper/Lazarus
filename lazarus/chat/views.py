01ebe60123925bd7de9190118883df6df94e3ce1
TheDarkBlueSnaper <mr.e.gegeckas@gmail.com>
2024-03-27 12:52:43 +0200

    Admin integration with chat, error in admin page

--- lazarus/chat/views.py
+++ lazarus/chat/views.py
@@ -1,10 +1,14 @@
 import json
 
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 
+from account.models import User
+
 from .models import Room
+
 
 @require_POST
 def create_room(request, uuid):
@@ -14,3 +18,26 @@ def create_room(request, uuid):
     Room.objects.create(uuid=uuid, client=name, url=url)
 
     return JsonResponse({'message': 'room created'})
+
+@login_required
+def admin(request):
+    rooms = Room.objects.all()
+    users = User.objects.filter(is_staff=True)
+
+    return render(request, 'chat/admin.html', {
+        'rooms': rooms,
+        'users': users
+    })
+
+@login_required
+def room(request, uuid):
+    room = Room.objects.get(uuid=uuid)
+
+    if room.status == Room.WAITING:
+        room.status = Room.ACTIVE
+        room.agent = request.user
+        room.save()
+
+    return render(request, 'chat/room.html', {
+        'room': room
+    })
